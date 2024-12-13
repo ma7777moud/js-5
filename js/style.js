733a8e2d2dd9e24eaf1443b52f8106e93d77437f
ma7777moud <mahmoudganaha2@gmail.com>
2024-12-13 18:04:08 +0200

    second commit

--- js/style.js
+++ js/style.js
@@ -1,29 +1,26 @@
 async function WeatherLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(async (position) => {
-            const lat = position.lat;
-            const lon = position.long;
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
             const apiKey = ' 87023d7c34a94e6089e200248240912';
-            const apiUrl =http//api.weatherapi.com/v1/search.json?key=87023d7c34a94e6089e200248240912&q=lond
-
-
+            const apiUrl ="https://api.weatherapi.com/v1/forecast.json?key=87023d7c34a94e6089e200248240912&q=lond&days=3"
             try {
                 function displayWeather(data) {
                     const weatherInfo = `
-                        <h2>Weather Info:</h2>
-                        <p>Location: ${data.city[0]}</p>
-                        <p>Temperature: ${data.list[0].main.temp}°C</p>
-                        <p>Condition: ${data.list[0].weather[0].description}</p>
+                        <h1>${data.location.name}</h1>
+                        <p>Date: ${data.forecast.forecastday[0].date}</p>
+                        <p>time: ${data.forecast.forecastday[0].astro.sunrise}</p>
+                        <p>text: ${data.current.condition.text}</p>
+                        <p> ${data.current.temp_f}</p>
+                        <p>clear</p>
                     `;
                     document.getElementById('weather-info').innerHTML = weatherInfo;
                 }
-                const data = await response.json();
-                displayWeather(data);
                 const response = await fetch(apiUrl);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                
+                const data = await response.json();
+                console.log(data);
+                displayWeather(data);                
             } catch (error) {
                 console.error('Error weather data:', error);
             }
